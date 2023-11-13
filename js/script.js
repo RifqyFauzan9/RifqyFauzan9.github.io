e8a2391168fbf00faaf575c736dc88b997adc05e
Rifqy Fauzan <r1fqyf4uz4n@gmail.com>
2023-11-13 13:18:35 +0700

    second commit

--- js/script.js
+++ js/script.js
@@ -74,3 +74,27 @@ const typed = new Typed('#im-a', {
     typeDelay: 500,
     loop: true
 });
+let currentIndex = 0;
+  const totalSlides = document.querySelectorAll('.slide').length;
+
+  function showSlide(index) {
+    const slides = document.querySelector('.slides');
+    if (index >= totalSlides) {
+      currentIndex = 0;
+    } else if (index < 0) {
+      currentIndex = totalSlides - 1;
+    } else {
+      currentIndex = index;
+    }
+
+    const displacement = -currentIndex * 100 + '%';
+    slides.style.transform = 'translateX(' + displacement + ')';
+  }
+
+  function prevSlide() {
+    showSlide(currentIndex - 1);
+  }
+
+  function nextSlide() {
+    showSlide(currentIndex + 1);
+  }
